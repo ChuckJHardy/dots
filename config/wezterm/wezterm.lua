db79aad2497208a2293aac43834acb9666ffb040
Chuck J Hardy <github@chuckjhardy.com>
2024-05-24 21:49:07 +0100

    Wezterm Rename Tab

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,13 +1,24 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
+local act = wezterm.action
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
-
--- This is where you actually apply your config choices
 
 config.color_scheme = "Tokyo Night Moon"
 config.font = wezterm.font("JetBrains Mono")
 
--- and finally, return the configuration to wezterm
+config.keys = {
+	{
+		key = "n",
+		mods = "CTRL|SHIFT",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+}
+
 return config

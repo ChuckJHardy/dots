e8df4f7e57c6970c8f861fe2be422add0e78aefc
Chuck J Hardy <github@chuckjhardy.com>
2024-05-24 20:13:57 +0100

    Homebrew

---
+++ install/brew-cast.sh
@@ -0,0 +1,11 @@
+# Install cask packages
+
+apps=(
+    alfred
+    visual-studio-code
+    vlc
+    wezterm
+)
+
+brew install "${apps[@]}" --cask
+
---
+++ install/brew.sh
@@ -0,0 +1,22 @@
+# Install Homebrew
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew update
+brew upgrade
+
+# Install packages
+
+apps=(
+    coreutils
+    git
+    tree
+    neovim
+    wget
+    starship
+)
+
+brew install "${apps[@]}"
+
+# Git comes with diff-highlight, but isn't in the PATH
+ln -sf "$(brew --prefix)/share/git-core/contrib/diff-highlight/diff-highlight" /usr/local/bin/diff-highlight
+

e1171c69993db32cd4a24744310c0cb9af2a40af
Chuck J Hardy <github@chuckjhardy.com>
2024-10-06 12:21:30 +0100

    Remove Title Bar from WezTerm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -30,4 +30,7 @@ config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 
+-- Window Config
+config.window_decorations = "NONE"
+
 return config

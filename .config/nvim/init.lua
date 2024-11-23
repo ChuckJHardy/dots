b30beed39600521a1612bc937027a11ac7a070bb
Chuck J Hardy <github@chuckjhardy.com>
2024-11-23 20:11:01 +0000

    Remove TSServer

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -784,9 +784,6 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
-        --
-
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
